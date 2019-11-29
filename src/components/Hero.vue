3315c4529aeb4afbcd20abb0de9135c5afb70174
virtualcursor <2540378-virtualcursor@users.noreply.gitlab.com>
2019-11-29 16:27:03 +0800

    Update hero.

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,14 +1,17 @@
 <template>
-  <section class="header hero is-large">
+  <section class="header hero">
     <div class="hero-body">
       <div class="container">
-        <div class="columns">
-          <div class="column is-offset-two-thirds is-one-third">
+        <div class="columns is-vcentered">
+          <div class="column is-one-third">
             <h5 class="subtitle">This is me</h5>
             <h2 class="title">Travor James</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin convallis, mauris
             non elementum commodo, lacus felis imperdiet mauris, varius tempor nibh ex at mauris.
             Proin facilisis tellus ut nisi commodo, et sagittis.</p>
+          </div>
+          <div class="column is-two-thirds has-text-centered">
+            <img src="@/assets/productivity.png" />
           </div>
         </div>
       </div>
